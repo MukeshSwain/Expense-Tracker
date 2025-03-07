7ffc3acd8befb108def6a82a5ff5d2e34ad16687
Mukesh Swain <161455068+MukeshSwain@users.noreply.github.com>
2025-03-07 09:13:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,7 @@ const port = process.env.PORT || 5001;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+app.use(cors());
 app.get("/",(req,res)=>{
     res.send("Hello World")
 })
